5a73b9f18b2bf5aebd28b784939d33baab3fd443
lyno4ka <yuliyamasterenko@gmail.com>
2019-10-09 22:36:33 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -5,19 +5,64 @@ document.addEventListener('DOMContentLoaded', () => {
 {
 const keyboardButton = document.querySelector('.search-form__keyboard');
 const keyboard = document.querySelector('.keyboard');
+const closeKeyboard = document.getElementById('close-keyboard');
+const searchInput = document.querySelector('.search-form__input');
 
-keyboardButton.addEventListener('click', () => {
+const toggleKeyboard = () => {
+  keyboard.style.top = keyboard.style.top ? '' : '50%';  
+};
 
-    keyboard.style.top = keyboard.style.top ? '' : '50%';
+const typing = (event) => {
+  const target = event.target;
+  
+  if (target.tagName.toLowerCase() === 'button') {
+    searchInput.value += target.textContent.trim();
+  }
+  //backspace
+  //пробел
+}
 
-})
+keyboardButton.addEventListener('click', toggleKeyboard);
+closeKeyboard.addEventListener('click', toggleKeyboard);
+keyboard.addEventListener('click', typing); 
 
 }
 
+//меню
+{
+
+    const burger = document.querySelector('.spinner');
+    const sidebarMenu = document.querySelector('.sidebarMenu');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        sidebarMenu.classList.toggle('rollUp');
+    })
+
+    sidebarMenu.addEventListener('click', (event) => {
+        let target = event.target;
+        target = target.closest('a[href="#"]');
+
+        if (target) {
+            const parentTarget = target.parentElement;
+            sidebarMenu.querySelectorAll('li').forEach((elem) => {
+                if (elem === parentTarget) {
+                    elem.classList.add('active');
+                } else {
+                    elem.classList.remove('active');
+                }
+            })
+        }
+    })
+
+}
+
+//модальное окно
+{ 
+    const divYoutuber = document.createElement('div');
 
 
-
-
+}
 
 
 });
