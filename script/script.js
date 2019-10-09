760f9718e0e427ee75872a0057dafef9569bfe00
lyno4ka <yuliyamasterenko@gmail.com>
2019-10-09 16:47:52 +0300

    Changes

---
+++ script/script.js
@@ -0,0 +1,21 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+
+//экранная клавиатура
+{
+const keyboardButton = document.querySelector('.search-form__keyboard');
+const keyboard = document.querySelector('.keyboard');
+
+keyboardButton.addEventListener('click', () => {
+    keyboard.style.top = '50%'; 
+})
+
+}
+
+
+
+
+
+
+
+});
