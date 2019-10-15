9ea1182a584901914808bf7a1430e805de4747ce
lyno4ka <yuliyamasterenko@gmail.com>
2019-10-15 17:41:20 +0300

    Removed API keys

--- script/script.js
+++ script/script.js
@@ -95,33 +95,283 @@ keyboard.addEventListener('click', typing);
             })
         }
     })
-
 }
 
-//модальное окно
-{ 
 
-  document.body.insertAdjacentHTML('beforeend', `
-    <div class="youTuberModal">
-        <div id="youtuberClose">&#215;</div>
-        <div id="youtuberContainer"></div>
-    </div>
-    `);
+const youtuber = () => {
 
-    
     const youtuberItems = document.querySelectorAll('[data-youtuber]');
     const youTuberModal = document.querySelector('.youTuberModal');
-
-  console.log(youTuberModal);
+    const youtuberContainer = document.getElementById('youtuberContainer');
     
+    const qw = [3840, 2560, 1920, 1280, 854, 640, 426, 256];
+    const qh = [2160, 1440, 720, 480, 360, 240, 144];
+
+     const sizeVideo = () => {
+        const ww = document.documentElement.clientWidth;
+        const wh = document.documentElement.clientHeight;
+
+        for (let i = 0; i < qw.length; i++){
+            if (ww > qw[i]) {
+                youtuberContainer.querySelector('iframe').style.cssText = `
+                  width: ${qw[i]}px;
+                  height: ${qh[i]}px;
+                `;
+                youtuberContainer.style.cssText = `
+                  width: ${qw[i]}px;
+                  height: ${qh[i]}px;
+                  top: ${(wh - qh[i]) / 2}px;
+                  left: ${(ww - qw[i]) / 2}px;
+                `;
+                break;
+            }
+        }
+    };
+
     youtuberItems.forEach(elem => {
         elem.addEventListener('click', () => {
             const idVideo = elem.dataset.youtuber;
-            divYoutuber.style.display = 'block';
+            youTuberModal.style.display = 'block';
 
+            const youTuberFrame = document.createElement('iframe');
+            youTuberFrame.src = `https://youtube.com/embed/${idVideo}`; 
+            youtuberContainer.insertAdjacentElement('beforeend', youTuberFrame);
+            
+            window.addEventListener('resize', sizeVideo);
+
+            sizeVideo();
         })
     })
+
+    youTuberModal.addEventListener('click', () => {
+        youTuberModal.style.display = '';
+        youtuberContainer.textContent = '';
+        window.removeEventListener('resize', sizeVideo);
+    });
 }
 
+
+// модальное окно
+{ 
+    document.body.insertAdjacentHTML('beforeend', `
+                    <div class="youTuberModal">
+                        <div id="youtuberClose">&#215;</div>
+                        <div id="youtuberContainer"></div>
+                    </div>
+                    `);
+    youtuber();
+}
+
+//API
+{
+    const API_KEY = '';
+    const CLIENT_ID = '';
+    
+    //авторизация
+    {
+
+        const buttonAuth = document.getElementById('authorize');
+        const authBlock = document.querySelector('.auth');
+        
+        const errorAuth = err => {
+                console.error(err);
+                authBlock.style.display = '';
+                };
+
+        const authenticate = () => gapi.auth2.getAuthInstance()
+                .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+                .then(() => console.log("Авторизация успешна!"))
+                .catch(errorAuth);
+          
+        const loadClient = () => {
+            gapi.client.setApiKey(API_KEY);
+            return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+                .then(() => console.log("GAPI client loaded for API"))
+                .then(() => authBlock.style.display = 'none')
+                .catch(errorAuth);
+          };
+
+          gapi.load("client:auth2", () => gapi.auth2.init({client_id: CLIENT_ID}));
+          
+
+
+          buttonAuth.addEventListener('click', () => {
+            authenticate().then(loadClient)
+          })    
+          
+    }
+
+    //request
+    {
+
+        const gloTube = document.querySelector('.logo-academy');
+        const trends = document.getElementById('yt_trend');
+        const like = document.getElementById('like');
+        const subscriptions = document.getElementById('subscriptions');
+        const searchForm = document.querySelector('.search-form');
+
+        const request = options =>  gapi.client.youtube[options.method]
+            .list(options)
+            .then(response => response.result.items)
+            .then(data => options.method === 'subscriptions' ? renderSub(data) : render(data))
+            .catch(err => console.error('Во время запроса произошла ошибка: ' + err))
+       
+        const renderSub = data => {
+            console.log(data);
+            const ytWrapper = document.getElementById('yt-wrapper');
+            ytWrapper.textContent = '';
+            data.forEach(item => {
+                console.log('sub');
+                try {
+                const {
+                    snippet: {
+                        resourceId: {
+                            channelId
+                        }, 
+                        description, 
+                        title, 
+                        thumbnails: {
+                            high: {
+                                url
+                            }
+                        }
+                    }
+                } = item;
+
+                ytWrapper.innerHTML += `
+                    <div class="yt" data-youtuber="${channelId}">
+                    <div class="yt-thumbnail" style="--aspect-ratio:16/9;">
+                    <img src="${url}" alt="thumbnail" class="yt-thumbnail__img">
+                    </div>
+                    <div class="yt-title">${title}</div>
+                    <div class="yt-channel">${description}</div>
+                    </div>
+                    `;
+                } catch (err) {
+                    console.error(err);
+                }
+            });
+            
+            ytWrapper.querySelectorAll('.yt').forEach(item => {
+                item.addEventListener('click', () => {
+                    request({
+                        method: 'search',
+                        part: 'snippet',
+                        channelId: item.dataset.youtuber,
+                        order: 'date',
+                        maxResults: 6,
+                    })     
+                })
+            });
+
+        };
+
+
+
+
+        const render = data => {
+            console.log(data);
+            const ytWrapper = document.getElementById('yt-wrapper');
+            ytWrapper.textContent = '';
+            data.forEach(item => {
+             try {
+                const {
+                    id, 
+                    id: {
+                        videoId
+                    }, 
+                    snippet: {
+                        channelTitle, 
+                        title, 
+                        resourceId: {
+                            videoId: likedVideoId
+                        } = {}, 
+                        thumbnails: {
+                            high: {
+                                url
+                            }
+                        }
+                    }
+                } = item;
+
+                ytWrapper.innerHTML += `
+                    <div class="yt" data-youtuber="${likedVideoId || videoId || id}">
+                    <div class="yt-thumbnail" style="--aspect-ratio:16/9;">
+                    <img src="${url}" alt="thumbnail" class="yt-thumbnail__img">
+                    </div>
+                    <div class="yt-title">${title}</div>
+                    <div class="yt-channel">${channelTitle}</div>
+                    </div>
+                    `;
+                } catch (err) {
+                    console.error(err);
+                }
+            });
+            youtuber();
+        };
+
+        gloTube.addEventListener('click', () => {
+            request({
+                method: 'search',
+                part: 'snippet',
+                channelId: 'UCVswRUcKC-M35RzgPRv8qUg',
+                order: 'date',
+                maxResults: 6,
+            })     
+        });
+
+
+        trends.addEventListener('click', () => {
+            request({
+                method: 'videos',
+                part: 'snippet',
+                chart: 'mostPopular',
+                maxResults: 6,
+                regionCode: 'UA',
+            })     
+        });
+
+        like.addEventListener('click', () => {
+            request({
+                method: 'playlistItems',
+                part: 'snippet',
+                playlistId: 'LLpDsnrXLzNDHhHhqBki-Ctw',
+                maxResults: 6,
+            })     
+        });
+
+        subscriptions.addEventListener('click', () => {
+            request({
+                method: 'subscriptions',
+                part: 'snippet',
+                mine: true,
+                maxResults: 6,
+            })  
+        });
+
+        searchForm.addEventListener('submit', event => {
+            event.preventDefault();
+            const valueInput = searchForm.elements[0].value;
+
+            if (!valueInput) {
+             searchForm.style.border = '1px solid red';
+             return;
+            }
+            searchForm.style.border = '';
+
+            request({
+                method: 'search',
+                part: 'snippet',
+                order: 'relevance',
+                maxResults: 6,
+                q: valueInput,
+            }) 
+
+            searchForm.elements[0].value = '';
+          
+        });
+
+    }
+}
 
 });
